0c31d33928d7342660f500ac5c089450d9409bc5
Hyeongchan Shin <intshc@gmail.com>
2023-06-07 15:18:51 +0900

    프로젝트 생성, config 작업중

---
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -0,0 +1,58 @@
+package com.example.config;
+
+import com.example.service.OAuth2UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+
+@Configuration
+@EnableMethodSecurity
+@EnableWebSecurity(debug = true)
+@RequiredArgsConstructor
+public class SecurityConfig{
+
+    private final OAuth2UserService oAuth2UserService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.authorizeHttpRequests(config -> config.anyRequest().permitAll());
+        http.oauth2Login(oauth2Configurer -> oauth2Configurer
+                .loginPage("/login")
+                .successHandler(successHandler())
+                .userInfoEndpoint()
+                .userService(oAuth2UserService));
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler successHandler() {
+        return ((request, response, authentication) -> {
+            DefaultOAuth2User defaultOAuth2User = (DefaultOAuth2User) authentication.getPrincipal();
+
+            String id = defaultOAuth2User.getAttributes().get("id").toString();
+            String body = """
+                    {"id":"%s"}
+                    """.formatted(id);
+
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+
+            PrintWriter writer = response.getWriter();
+            writer.println(body);
+            writer.flush();
+        });
+    }
+}
